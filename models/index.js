372d960def70b8e2689e1f80e118712829e433ff
JamesRileydev <jamesriley.dev@gmail.com>
2019-03-22 15:41:10 -0400

    Refactored route functions

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-
+mongoose.connect('mongodb://jriley:Express!1@ds221416.mlab.com:21416/express_api', 
+    {useNewUrlParser: true});
 
 mongoose.Promise = Promise;
 
