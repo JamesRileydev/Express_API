7fc08786cb6ca2dd5cadf2450a572e6c000b4d74
unknown <you@example.com>
2019-04-27 18:06:34 -0400

    removed unwanted lines

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://jriley:Express!1@ds221416.mlab.com:21416/express_api', 
+mongoose.connect('', 
     {useNewUrlParser: true});
 
 mongoose.Promise = Promise;
